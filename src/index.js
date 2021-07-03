c99d92e3e8aacae989add255a904dd2442b88517
염규석 <yeumkyuseok@gmail.com>
2021-07-03 23:04:09 +0900

    aws command deleted

--- src/index.js
+++ src/index.js
@@ -9,9 +9,6 @@ import About from "./pages/about"
 import Post from "./pages/post"
 import NotFound from "./pages/notfound"
 import "./global.css"
-import Amplify from "aws-amplify";
-import awsExports from "./aws-exports";
-Amplify.configure(awsExports);
 
 ReactDOM.render(
   <Router basename={process.env.PUBLIC_URL}>
